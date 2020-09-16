6124f26560ded4308345e9a6971898185a038f3d
ADMINX\Administrator <alc1kinhdoanh@gmail.com>
2020-09-16 11:13:45 +0700

    git co ban

--- src/com/cangido/Main.java
+++ src/com/cangido/Main.java
@@ -11,5 +11,19 @@ public class Main {
         System.out.println("tuyet voi con voi");
 
         System.out.println("test git ok");
+
+        System.out.println("Git cơ bản!");
+        System.out.println("git init    Khởi tạo 1 lần đâu");
+        System.out.println("git add .   Thêm tất cả thay đổi trong mọi file");
+        System.out.println("git commit -m \"ghi chú cho commit\" ");
+        System.out.println("Tạo new repository tren github.com và sẽ có đường dẫn *.git");
+        System.out.println("git remote add origin <PASTE GITHUB URL>\n");
+        System.out.println("git remote add origin https://github.com/codetql/hellojava.git");
+        System.out.println("Your local Git repository is now connected to GitHub.");
+        System.out.println("git push origin master      //push to GitHub. ");
+
+
+        System.out.println("Clone from GitHub\n");
+        System.out.println("git clone <PASTE CLONE URL>\n");
     }
 }
